719353b702a34be0c6314baf4fe7e31157470857
Jason Cameron <dev.jasoncameron@gmail.com>
2024-05-12 18:05:00 -0500

    some lsns comple

--- Trash/Lua/main.lua
+++ Trash/Lua/main.lua
@@ -1,4 +1,25 @@
--- test
---- Testing Lua
----
 
+
+--local count = 0
+----while count < 10 do
+----    count = count + 1
+----    print("smash")
+----end
+--
+--repeat
+--    count = count + 1
+--    print("smash")
+--until count > 0
+
+--io.write("What is your name? ")
+--local ans = io.read()
+--
+--if ans == "Smash" then
+--    print("Smash is Amazing!!!")
+--else
+--    print(ans .."? To bad, I was hoping it was Smash.")
+--end
+
+local tbl = {
+
+}
